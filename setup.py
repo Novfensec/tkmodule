106eb9da5c55062fa9d8692754db11a1370683cc
Kartavya Shukla <87070473+Novfensec@users.noreply.github.com>
2022-09-03 17:42:53 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup
 setup(
   name = 'tkmodule',         # How you named your package folder (MyLib)
   packages = ['tkmodule'],   # Chose the same as "name"
-  version = '0.1.4',      # Start with a small number and increase it with every change you make
+  version = '0.1.5',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'Tkinter GUI Utility. Compress large tkinter projects to small files using this.',   # Give a short description about your library
   author = 'Kartavya Shukla',                   # Type in your name
   author_email = 'novfensec@protonmail.com',      # Type in your E-Mail
   url = 'https://novfensec.herokuapp.com/',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/Novfensec/tkmodule/archive/refs/tags/v_0.1.4.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/Novfensec/tkmodule/archive/refs/tags/v_0.1.5.tar.gz',    # I explain this later on
   keywords = ['Tkinter', 'tkmodule', 'TkinterUtitlity'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'validators',
