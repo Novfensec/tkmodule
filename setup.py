f00f009dbea02808c51c453c12ab61dd31374701
Kartavya Shukla <87070473+Novfensec@users.noreply.github.com>
2024-06-12 12:17:59 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md') as file:
 setup(
   name = 'tkmodule',         # How you named your package folder (MyLib)
   packages = ['tkmodule'],   # Chose the same as "name"
-  version = '0.1.6',      # Start with a small number and increase it with every change you make
+  version = '0.3.3',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'TkModule equips developers with a suite of tools and functions that elevate the Tkinter experience to new heights, making GUI creation more intuitive, faster, and more enjoyable. TkModule comes in offering a simplified and more Pythonic approach to GUI development.',   # Give a short description about your library
   long_description=long_description,
@@ -13,7 +13,7 @@ setup(
   author = 'Kartavya Shukla',                   # Type in your name
   author_email = 'novfensec@protonmail.com',      # Type in your E-Mail
   url = 'https://novfensec.vercel.app/projects/tkmodule',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/Novfensec/tkmodule/archive/refs/tags/v_0.1.6.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/Novfensec/tkmodule/archive/refs/tags/v_0.3.3.tar.gz',    # I explain this later on
   keywords = ['Tkinter', 'tkmodule', 'TkinterUtitlity'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'validators',
